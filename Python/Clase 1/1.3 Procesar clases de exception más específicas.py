270613d9d8cab4d94831d9c545cf72164b763bdf
Nicolas5718 <164242580+Nicolas5718@users.noreply.github.com>
2026-04-06 23:00:36 -0300

    Creado el 1.3

---
+++ Python/Clase 1/1.3 Procesar clases de exception más específicas.py
@@ -0,0 +1,10 @@
+resultado = None 
+a = 10
+b = 0
+try:    
+    resultado = a / b
+except Exception as e:
+    Print(f'Ocurrio un error: {e}')
+
+    print(f'El resultado es: {resultado}')
+    print('seguimos...')
