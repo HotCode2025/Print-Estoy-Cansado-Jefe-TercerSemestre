313a10d15aaea7dab6a7e121d77158aa746ed914
RoyBruna <roybruna1580@gmail.com>
2026-04-15 20:44:34 -0300

    1.3 Video 4

---
+++ Java/Clase 2/1.3 Pruebas de enum, con la creación de enum Continentes.java
@@ -0,0 +1,39 @@
+package test;
+
+import enumeraciones.Dias;
+
+public class TestEnumeraciones {
+    public static void main(String[] args) {
+        System.out.println("Dia 1: " + Dias.LUNES);
+        indicarDiaSemana(Dias.LUNES);
+    }
+
+    private static void indicarDiaSemana(Dias dias) {
+        switch(dias){
+            case LUNES:
+                System.out.println("Primer día de la semana");
+                break;
+            case MARTES:
+                System.out.println("Segundo día de la semana");
+                break;
+            case MIERCOLES:
+                System.out.println("Tercer día de la semana");
+                break;
+            case JUEVES:
+                System.out.println("Cuarto día de la semana");
+                break;
+            case VIERNES:
+                System.out.println("Quinto día de la semana");
+                break;
+            case SABADO:
+                System.out.println("Sexto día de la semana");
+                break;
+            case DOMINGO:
+                System.out.println("Séptimo día de la semana");
+                break;
+            default:
+                System.out.println("Día no válido");
+                break;
+        }
+    }
+}
