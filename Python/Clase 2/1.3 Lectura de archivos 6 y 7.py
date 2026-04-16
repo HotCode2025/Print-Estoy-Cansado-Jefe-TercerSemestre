04dac91e53803af6a1e66c1b68f26cda5229f62e
Franco Fernandez <164897425+ffrancode@users.noreply.github.com>
2026-04-15 23:08:18 -0300

    Agregamos ejercicio 1.3 de Python | Clase 2 Programacion III Lunes 2026

---
+++ Python/Clase 2/1.3 Lectura de archivos 6 y 7.py
@@ -0,0 +1,15 @@
+# Ejercicio 1.3 del video 6 con Python
+
+archivo = open('prueba.txt', 'r', encoding='utf8') # las letras son: 'r' read, 'a' append, 'w' write, 'x'
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10)) # Continuamos desde la linea anterior
+print(archivo.readline())
+print(archivo.readline())
+
+
+
+# En Ejercicio 1.3 del video 7 con Python aprendemos como especificar la ruta en caso de que el.. 
+# archivo no esté en la carpeta donde estamos trabajando.
+# Y usamos doble barra inversa \\ para que no lo tome como simbolo especial, luego si en las subcarpetas usamos \
+
