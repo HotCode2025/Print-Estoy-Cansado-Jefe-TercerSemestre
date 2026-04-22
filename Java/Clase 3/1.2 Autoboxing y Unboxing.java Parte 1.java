b4a9191872a7f6076cb8f1912e7b7542dab663bd
Gian Franco <gianfranco321123@gmail.com>
2026-04-22 20:46:51 -0300

    Create 1.2 Autoboxing y Unboxing.java Parte 1.java
    subo clase sobre Autoboxing y Unboxing

---
+++ Java/Clase 3/1.2 Autoboxing y Unboxing.java Parte 1.java
@@ -0,0 +1,14 @@
+public class AutoboxingUnboxing {
+    public static void main(String[] args) {
+        //clase envolvente o wrapper
+        /*
+        Clases envolventes de tipos primitivos
+        int = la clase envolvente es Integer
+         */
+        int enteroPrim = 10; //Tipo primitivo
+        System.out.println("enteroPrim = " + enteroPrim);
+        Integer entero = 10; // Tipo object con la clase Integer
+        System.out.println("entero = " + entero);
+    }
+
+}
