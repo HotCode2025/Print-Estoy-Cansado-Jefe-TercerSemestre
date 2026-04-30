4d78cb7cca1c1aaed8a23ef1127006603aed0d69
farreceygor <129985516+farreceygor@users.noreply.github.com>
2026-04-30 00:29:39 -0300

    Clase 3 videos 5 y 6

---
+++ Java/Clase 3/1.3 Modificadores de acceso public Clase1.java
@@ -0,0 +1,13 @@
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublic = "Valor atributo public";
+    
+    public Clase1(){
+        System.out.println("Contructor public");
+    }
+    
+    public void metodoPublico(){
+        System.out.println("Método public");
+    }
+}
