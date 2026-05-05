20c1020b443780bf0ccd5bbcee18b16ebbbfbc48
Gian Franco <gianfranco321123@gmail.com>
2026-05-04 21:18:34 -0300

    Create 5.1 Uso_de_with_y_psycopg2.py
    subo clase 5.1

---
+++ Python/Clase 5/5.1 Uso_de_with_y_psycopg2.py
@@ -0,0 +1,20 @@
+import psycopg2 # Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect(user='postgres',
+                            password='admin',
+                            host='127.0.0.1',
+                            port='5432',
+                            database='test_bd') #podemos ponerlo en 1 linea o así, según la preferencia de legibilidad y orden o cantidad de lineas
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona' #Placeholder
+            cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchall() # Recuperamos todos los registros que serán una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.html
