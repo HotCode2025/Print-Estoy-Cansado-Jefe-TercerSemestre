a8b036e8b0ddaca8394d71c2a0c60cea81462aa1
Franco Fernandez <164897425+ffrancode@users.noreply.github.com>
2026-05-11 21:59:24 -0300

    Agregamos clase 6.1 JS Clase 6

---
+++ Javascript/Clase 6/6.1-funcionesCallback.js
@@ -0,0 +1,23 @@
+miFuncion1();
+miFuncion2();
+
+function miFuncion1() {
+  console.log('Función 1');
+}
+
+function miFuncion2() {
+  console.log('Función 2');
+}
+
+// Función de tipo callback
+let impr = function imprimir(mensaje) {
+  console.log(mensaje);
+}
+
+function sumar(op1, op2, funcionCallback) {
+  let resultado = op1 + op2;
+  funcionCallback(`El resultado es: ${resultado}`);
+}
+
+// Llamada con callback
+sumar(5, 3, impr);
