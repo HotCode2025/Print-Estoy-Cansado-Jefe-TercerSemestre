379fe9bc97d227d14957edf9b2708d977e87b769
farreceygor <129985516+farreceygor@users.noreply.github.com>
2026-05-17 16:40:57 -0300

    Correccion nombres Clase 6.2 Python

--- Python/Clase 6/6.2 Manejo de transacciones (Manualmente) Parte 3.py
+++ Python/Clase 6/6.2 Manejo de transacciones (Manualmente) Parte 3.py
@@ -2,13 +2,21 @@ import psycopg2 as bd # Esto es para poder conectarnos a Postgre
 
 conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
 try:
-    conexion.autocommit = False
+    conexion.autocommit = False # esto directamente no debería estar
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
-    valores = ('Maria', 'Esparza', 'mesparza@mail.com')
+    valores = ('Carlos', 'Lara', 'carloslara123@email.com')
     cursor.execute(sentencia, valores)
-    print('Termina la transaccion')
+
+    sentencia = "UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s"
+    valores = ("Juan", "Juarez", "jcjuarez@email.com", 1)
+    cursor.execute(sentencia, valores)
+
+
+    conexion.commit() # Hacemos el commit manualmente. se cierra la transaccion
+    print('Termina la transacción')
 except Exception as e:
-    print(f'Ocurrio un error: {e}')
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
 finally:
     conexion.close()
