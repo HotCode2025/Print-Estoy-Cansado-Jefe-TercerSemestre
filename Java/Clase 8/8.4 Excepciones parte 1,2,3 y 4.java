0b83d61c55fb9c4dfa48a84acc54613aff23dc52
Martin0774 <martinplatero07@gmail.com>
2026-05-27 21:42:28 -0300

    subo 8.4 Excepciones parte 1,2,3 y 4.java

---
+++ Java/Clase 8/8.4 Excepciones parte 1,2,3 y 4.java
@@ -0,0 +1,14 @@
+package test;
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 0;
+        try {
+            resultado = 10/0;
+        }catch(Exception e) {
+            System.out.println("Ocurrio un error");
+            e.printStackTrace(System.out); //Se conoce como pila de excepciones
+        }
+        System.out.println("La variable de resultado tiene como valor: " + resultado);
+    }
+}
