94c599f13f39bc6a98112e679d8182073eae2b32
Fabio Arreceygor <arreceygorfabio@gmail.com>
2026-06-27 20:14:46 -0300

    cambie las rutas de App.py para leer preguntas desde SQLite en lugar de memoria

--- Proyecto Integrador Python/TRIVIA SIMPSON PYTHON/Proyecto Trivia Simpsons/App.py
+++ Proyecto Integrador Python/TRIVIA SIMPSON PYTHON/Proyecto Trivia Simpsons/App.py
@@ -1,15 +1,15 @@
 import os
 import sqlite3
 from flask import Flask, render_template
+from models.Pregunta import Pregunta
 
 app = Flask(__name__)
 
 def get_db_connection():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(base_dir, 'database', 'trivia.db')
-    
     conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row  # Esto nos permite acceder a las columnas por nombre como un diccionario
+    conn.row_factory = sqlite3.Row
     return conn
 
 @app.route("/")
@@ -21,7 +21,7 @@ def inicio():
 def jugar(pregunta_id=None):
     conn = get_db_connection()
     
-    # 1. Si no viene un ID, buscamos la primera pregunta de la base de datos
+    # Buscar la primera pregunta si no viene ID
     if pregunta_id is None:
         primer_registro = conn.execute("SELECT id FROM preguntas ORDER BY id ASC LIMIT 1").fetchone()
         if primer_registro:
@@ -29,12 +29,14 @@ def jugar(pregunta_id=None):
         else:
             return render_template("jugar.html", pregunta=None, siguiente_id=None)
             
-    # 2. Buscar la pregunta actual por su ID
-    pregunta_obj = conn.execute("SELECT * FROM preguntas WHERE id = ?", (pregunta_id,)).fetchone()
+    #Traer la fila de la BD
+    fila_pregunta = conn.execute("SELECT * FROM preguntas WHERE id = ?", (pregunta_id,)).fetchone()
     
-    # 3. Buscar el ID de la siguiente pregunta que exista en la BD (por si hay saltos de ID)
+    #Convertimos la fila en un Objeto Pregunta
+    pregunta_obj = Pregunta.mapear_desde_bd(fila_pregunta)
+    
+    #Buscar el ID de la siguiente pregunta
     siguiente_registro = conn.execute("SELECT id FROM preguntas WHERE id > ? ORDER BY id ASC LIMIT 1", (pregunta_id,)).fetchone()
-    
     siguiente_id = siguiente_registro['id'] if siguiente_registro else None
 
     conn.close()
